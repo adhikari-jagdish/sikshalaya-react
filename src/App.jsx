5a5e0f8638c8ae6ed18e64c0000041ca325930d3
Jagadish <adhikari.jagdish5200@gmail.com>
2025-07-16 10:28:47 +0545

    Links added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Dashboard from './dashboard.jsx'
 import Header from './header.jsx'
 import Footer from './footer.jsx'
 import './styles.css'
+import AboutUs from './AboutUs.jsx'
+import Courses from './Courses.jsx'
+import Admissions from './Admissions.jsx'
+import ContactUs from './ContactUs.jsx'
 
 function App() {
   return (
@@ -13,7 +17,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
-
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/admissions" element={<Admissions />} />
+          <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
         <Footer />
       </div>
--- src/header.jsx
+++ src/header.jsx
@@ -5,11 +5,11 @@ function Header() {
         <header>
             <div class="title">Sikshalaya College</div>
             <nav class="menu">
-                <a href='#'>Home</a>
-                <a href='#'>About Us</a>
-                <a href='#'>Courses</a>
-                <a href='#'>Admissions</a>
-                <a href='#'>Contact Us</a>
+                <a href='/'>Home</a>
+                <a href='/about-us'>About Us</a>
+                <a href='/courses'>Courses</a>
+                <a href='/admissions'>Admissions</a>
+                <a href='/contact-us'>Contact Us</a>
             </nav>
             <div class = "logout">
                 <button>Logout</button>
